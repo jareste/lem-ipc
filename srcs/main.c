01ac1f5ebf308b881bd0a0327549f7c2810def30
jareste <jareste2000@gmail.com>
2024-11-17 15:27:50 +0100

    added teams and teams only receiving their teams messages, noice

--- srcs/main.c
+++ srcs/main.c
@@ -7,13 +7,22 @@
 #include <sys/sem.h>
 #include <sys/msg.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define SHM_KEY 0x1234
 #define SEM_KEY 0x5678
-#define MSG_KEY 0x9ABC
+#define MSG_KEY_BASE 0x9ABC
+#define MAX_TEAMS 21
 
-int shm_id, sem_id, msg_id;
+int shm_id, sem_id;
 int *shm_ptr = NULL;
+int msg_ids[MAX_TEAMS] = {0};
+
+struct message
+{
+    long mtype;
+    char mtext[128];
+};
 
 void lock_semaphore()
 {
@@ -41,20 +50,25 @@ void cleanup()
 
     if (*shm_ptr == 1)
     {
-        printf("Last process: Cleaning up resources.\n");
-        unlock_semaphore();
+        printf("\nLast process: Cleaning up resources.\n");
         shmctl(shm_id, IPC_RMID, NULL);
         semctl(sem_id, 0, IPC_RMID);
-        msgctl(msg_id, IPC_RMID, NULL);
+        for (int i = 0; i < MAX_TEAMS; i++)
+        {
+            if (msg_ids[i] != 0)
+            {
+                msgctl(msg_ids[i], IPC_RMID, NULL);
+            }
+        }
+        shmdt(shm_ptr);
     }
     else
     {
         (*shm_ptr)--;
-        printf("Detached. Remaining processes: %d\n", *shm_ptr);
+        printf("\nDetached. Remaining processes: %d\n", *shm_ptr);
+        shmdt(shm_ptr);
         unlock_semaphore();
     }
-
-    shmdt(shm_ptr);
     exit(0);
 }
 
@@ -63,25 +77,138 @@ void force_cleanup()
     printf("Force cleaning up all shared resources.\n");
     shmctl(shm_id, IPC_RMID, NULL);
     semctl(sem_id, 0, IPC_RMID);
-    msgctl(msg_id, IPC_RMID, NULL);
+    for (int i = 0; i < MAX_TEAMS; i++)
+    {
+        if (msg_ids[i] != 0)
+        {
+            msgctl(msg_ids[i], IPC_RMID, NULL);
+        }
+    }
     exit(0);
 }
 
 void handle_sigint(int sig)
 {
-    (void)sig;
+    (void)sig; // Suppress unused warning
     cleanup();
+}
+
+int initialize_msg_queues()
+{
+    for (int i = 0; i < MAX_TEAMS; i++)
+    {
+        msg_ids[i] = msgget(MSG_KEY_BASE + i, IPC_CREAT | 0666);
+        if (msg_ids[i] == -1)
+        {
+            perror("msgget");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void send_message(int team, const char *message, long mtype)
+{
+    if (team < 0 || team >= MAX_TEAMS)
+    {
+        fprintf(stderr, "Invalid team number.\n");
+        return;
+    }
+
+    struct message msg;
+    msg.mtype = mtype;
+    strncpy(msg.mtext, message, sizeof(msg.mtext));
+
+    if (msgsnd(msg_ids[team], &msg, sizeof(msg.mtext), 0) == -1)
+    {
+        perror("msgsnd");
+    }
+}
+
+void broadcast_message(int team, const char *message)
+{
+    if (team < 0 || team >= MAX_TEAMS)
+    {
+        fprintf(stderr, "Invalid team number.\n");
+        return;
+    }
+
+    struct message msg;
+    strncpy(msg.mtext, message, sizeof(msg.mtext));
+
+    for (int i = 0; i < MAX_TEAMS; i++)
+    {
+        msg.mtype = i + 1; // Use process-specific mtype
+        if (msgsnd(msg_ids[team], &msg, sizeof(msg.mtext), 0) == -1)
+        {
+            perror("msgsnd (broadcast)");
+        }
+    }
+}
+
+void receive_message(int team, long mtype)
+{
+    if (team < 0 || team >= MAX_TEAMS)
+    {
+        fprintf(stderr, "Invalid team number.\n");
+        return;
+    }
+
+    struct message msg;
+    if (msgrcv(msg_ids[team], &msg, sizeof(msg.mtext), mtype, IPC_NOWAIT) == -1)
+    {
+        if (errno != ENOMSG)
+        {
+            perror("msgrcv");
+        }
+    }
+    else
+    {
+        printf("Received from team %d: %s\n", team, msg.mtext);
+    }
+}
+
+void get_team_number(char *s, int *team)
+{
+    if (strlen(s) > 1)
+        goto error;
+    
+    if (s[0] < '0' || s[0] > '9')
+        goto error;
+    
+    *team = atoi(s);
+
+    /* should never happen lol */
+    if (*team < 0 || *team >= MAX_TEAMS)
+        goto error;
+    
+    return;
+error:
+    fprintf(stderr, "Invalid team number. Valids are [0 - 9]\n");
+    exit(EXIT_FAILURE);
+
 }
 
 int main(int argc, char *argv[])
 {
-    if ((argc > 1) && (strcmp(argv[1], "--clean") == 0 || strcmp(argv[1], "-c") == 0))
+    int team = 0;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s [--clean] [team]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((strcmp(argv[1], "--clean") == 0 ) || (strcmp(argv[1], "-c") == 0))
     {
         shm_id = shmget(SHM_KEY, sizeof(int), 0666);
         sem_id = semget(SEM_KEY, 1, 0666);
-        msg_id = msgget(MSG_KEY, 0666);
+        for (int i = 0; i < MAX_TEAMS; i++)
+        {
+            msg_ids[i] = msgget(MSG_KEY_BASE + i, 0666);
+        }
 
-        if (shm_id == -1 || sem_id == -1 || msg_id == -1)
+        if (shm_id == -1 || sem_id == -1)
         {
             perror("Error accessing shared resources");
             exit(EXIT_FAILURE);
@@ -89,6 +216,8 @@ int main(int argc, char *argv[])
 
         force_cleanup();
     }
+
+    get_team_number(argv[1], &team);
 
     signal(SIGINT, handle_sigint);
 
@@ -118,11 +247,9 @@ int main(int argc, char *argv[])
         semctl(sem_id, 0, SETVAL, 1);
     }
 
-    msg_id = msgget(MSG_KEY, IPC_CREAT | 0666);
-    if (msg_id == -1)
+    if (initialize_msg_queues() == -1)
     {
-        perror("msgget");
-        exit(EXIT_FAILURE);
+        cleanup();
     }
 
     lock_semaphore();
@@ -137,10 +264,16 @@ int main(int argc, char *argv[])
 
     unlock_semaphore();
 
+    printf("Joined team %d\n", team);
+
+    char join_message[128];
+    snprintf(join_message, sizeof(join_message), "Process %d has joined the team.", getpid());
+    send_message(team, join_message, 1);
+
     while (1)
     {
-        printf("Process %d running...\n", getpid());
         sleep(1);
+        receive_message(team, 1);
     }
 
     return 0;
